f3e4607624b6175a8e48366b1df9ca8eb9bd6da5
Tien-Dattt <nguyentiendatg286@gmail.com>
2025-06-20 16:38:39 +0700

    [Fix] tyxemlai

--- __manifest__.py
+++ __manifest__.py
@@ -9,27 +9,24 @@
     'description': """
         Long description of module's purpose
     """,
-
+    'sequence': '1',
     'author': "My Company",
     'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Inventory',
     'version': '0.1',
-
-    # any module necessary for this one to work correctly
     'depends': ['base','stock'],
-
-    # always loaded
     'data': [
         'security/ir.model.access.csv',
         'wizard/wizard_import_kiemke_view.xml',
         'views/inventory_check.xml',
+        'wizard/wizard_product_view.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
 }
---
+++ wizard/wizard_product_line.py
@@ -0,0 +1,42 @@
+from odoo import models, fields, api
+
+class ProductSelectionLineWizard(models.TransientModel):
+    _name = 'product.selection.line.wizard'
+    _description = ''
+
+    # wizard_id = fields.Many2one('product.selection.wizard', string='Wizard')
+    product_id = fields.Many2one('product.product', string='Product')
+    internal_reference = fields.Char(string='Internal Reference')
+    location_id = fields.Many2one('stock.location', string='Location')
+    check_id = fields.Many2one('intern_inventory.check', string="Inventory")
+    lot_id = fields.Many2one('stock.lot', string='Lot/Serial Number')
+    quantity = fields.Float(string='Available Quantity')  # Quantity from the quant
+    is_selected = fields.Boolean(string='Select', default=False)
+
+    def action_confirm(self):
+        check_id = self.env.context.get('default_check_id')
+        if not check_id:
+            return
+
+        check = self.env['intern_inventory.check'].browse(check_id)
+
+        for line in self:
+            lot_id = False
+            quant = self.env['stock.quant'].search([
+                ('product_id', '=', line.product_id.id),
+                ('location_id', '=', line.location_id.id),
+                ('lot_id', '!=', False),
+            ], limit=1)
+            if quant:
+                lot_id = quant.lot_id.id
+
+            self.env['inventory.line'].create({
+                'check_id': check.id,
+                'product_id': line.product_id.id,
+                'location_id': line.location_id.id,
+                'lot_id': lot_id,
+                'uom_id': line.product_id.uom_id.id,
+                'quantity_counted': 0.0,
+            })
+
+        return {'type': 'ir.actions.act_window_close'}
